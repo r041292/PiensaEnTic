b30ccd545b3672063159c5fc028f7d45d83c2c1a
Demo <0041292@gmail.com>
2014-08-30 19:58:26 -0500

    init

---
+++ piensaEnTic/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PiensaEnTic::Application.config.secret_key_base = 'caad660e26a4a0e7ebc5425dea07fcdc5fa5789cd9369dacfced4e373e4ee5885b60eb5cb9c9217a1f211fcab86b21cd2869be3ee3bc515d96bacf243b7c660c'
