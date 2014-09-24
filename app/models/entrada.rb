47e797b9d055a0c48a54d3e9c68e4a91d4251956
Demo <0041292@gmail.com>
2014-09-23 22:11:53 -0500

    Update en el modelo de entradas.

--- app/models/entrada.rb
+++ app/models/entrada.rb
@@ -1,8 +1,8 @@
 class Entrada < ActiveRecord::Base
 	validates :nombre, presence: true, format:{ with: /\A([a-zA-Z]| )+\z/, message: "Solo se admiten letras" }
-	validates :email, presence: true, format:{ with: /\A[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]+\z/, message: "No es un email valido" }
+	validates :email, presence: true, format:{ with: /\A[a-zA-Z0-9]+@[a-zA-Z0-9]+(.[a-zA-Z]+)+\z/, message: "No es un email valido" }
 	validates :colegio, presence: true, format:{ with: /\A([a-zA-Z]| )+\z/, message: "Solo se admiten letras" }
-	validates :nivel, presence: true, format:{ with: /[0-9]+/, message: "Solo se admiten numeros" }
+	validates :nivel, presence: true
 	validates :ciudad, presence: true, format:{ with: /\A([a-zA-Z]| )+\z/, message: "Solo se admiten letras" }
 	validates :link_video, presence: true, format:{ with: /\A(http:\/\/|https:\/\/)(\w|\W)+\z/, message: "No es una URL Valida" }
 	validates :link_bloopers, :allow_blank => true, format:{ with: /\A(http:\/\/|https:\/\/)(\w|\W)+\z/, message: "No es una URL Valida" }
